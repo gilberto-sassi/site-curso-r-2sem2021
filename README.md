bd036fb0449e6f75d1db8552f81948a1fbd75c1f
Gilberto Sassi <sassi.pereira.gilberto@gmail.com>
2021-11-13 20:20:52 -0300

    adding files to 'material' section

---
+++ README.md
@@ -0,0 +1,2 @@
+# site-curso-r-2sem2021
+Site de curso livre R (2sem2021).
---
+++ build/js/forms.js
@@ -0,0 +1,88 @@
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('uf').value = ("");
+    // document.getElementById('ibge').value = ("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+        // document.getElementById('ibge').value = (conteudo.ibge);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function pesquisaCEP(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    const cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        const validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
+            // document.getElementById('ibge').value = "...";
+
+            //Cria um elemento javascript.
+            const script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            const elementos = document.getElementsByClassName('mostrar');
+
+            // for (let i = 0; i < elementos.length; i++) {
+            //     elementos[i].style.display = "block";
+            // }
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
+};
+
+function disableSubmit() {
+    document.getElementById("submit").disabled = true;
+}
+
+function activateButton(element) {
+
+    if (element.checked) {
+        document.getElementById("submit").disabled = false;
+    }
+    else {
+        document.getElementById("submit").disabled = true;
+    }
+
+}
